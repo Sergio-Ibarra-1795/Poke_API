75f76723cdc9209306c29b007ee9364868912ba3
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-10-05 04:28:32 -0500

    POKE API FIRST APPROACH

--- Poke_API/Poke_API.js
+++ Poke_API/Poke_API.js
@@ -54,7 +54,6 @@ fetch(url)
     return resultadoFetch2.json()
 })
 .then(respuestaJson2=>{
-    //console.log(respuestaJson2)
     const {results, previous, next} = respuestaJson2
     prevURL = previous
     nextURL = next
@@ -143,45 +142,6 @@ const handleListItemClick =(e)=>{
     fetchPokeData(idIndividual)
 
 }
-
-
-//To fetch the POKEAPI and "transform" the result into a JSON 
-//Fetch for the left side of the pokedex
-fetch('https://pokeapi.co/api/v2/pokemon/100')
-.then(resultadoFetch1=>{
-    return resultadoFetch1.json()
-})
-.then(respuestaJson=>{
-    resetScreen()
-/*Section to populate the left section of the pokedex and linked 
-the result of the API fetch (which is converted to a JSON) be the data
-that populates the pone name, type, etc with the DOM
-*/
-    const pokemontypes = respuestaJson['types']
-    const pokemonFirstType = pokemontypes[0]
-    const pokemonSecondType = pokemontypes[1]
-    pokeTypeOne.textContent = capitalize(pokemonFirstType['type']['name'])
-    if (pokemonSecondType){
-        pokeTypeTwo.classList.remove('hide')
-        pokeTypeTwo.textContent = capitalize(pokemonSecondType['type']['name'])
-    } else {
-        pokeTypeTwo.classList.add('hide')
-        pokeTypeTwo.textContent = ''
-    }
-
-    mainScreen.classList.add(pokemonFirstType['type']['name'])
-
-    
-    pokeName.textContent = capitalize(respuestaJson['name'])
-    pokeId.textContent ='#'+ respuestaJson['id'].toString().padStart(3,'0')
-    pokeWeight.textContent = respuestaJson['weight']
-    pokeHeigt.textContent = respuestaJson['height']
-
-
-    pokeFrontImage.src = respuestaJson['sprites']['front_default'] || ""
-    pokeBackImage.src = respuestaJson['sprites']['back_default'] || ""
-
-})
 
 
 //Adding event listeners 
